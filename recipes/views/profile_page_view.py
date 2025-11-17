5c4350aac56a56bf624a862d2154e26892e480e5
Hosannah Kibreab <hosannah.kibreab@kcl.ac.uk>
2025-11-17 22:00:48 +0000

    add favoutite recipe functionality

--- recipes/views/profile_page_view.py
+++ recipes/views/profile_page_view.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from math import floor
 
+from recipes.models import Favourite
+from recipes.models import Recipe
+
 recipes = [
     {
         'title': 'Chicken Stroganoff',
@@ -42,10 +45,20 @@ def profile_page(request):
     half_star = rating-full_stars==0.5
     empty_stars = 5 - full_stars - half_star
 
+
+    favourite_recipes = []
+    favourite_recipe_ids = Favourite.objects.filter(user=current_user).values_list('recipe', flat=True)
+    for item in Recipe.objects.all():
+        if item.pk in favourite_recipe_ids:
+            favourite_recipes.append(item)
+
+
+
     return render(request, 'profile_page.html', {
         'user': current_user,
         'recipes':recipes,
         'full_stars': range(full_stars),
         'half_star': half_star,
-        'empty_stars': range(empty_stars)
+        'empty_stars': range(empty_stars),
+        'favourite_recipes':  favourite_recipes
         })
