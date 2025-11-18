1633e3c81805629637e3c0b5348b751c0a714c6e
Hiu-Wai <chloeho248@gmail.com>
2025-11-18 19:05:56 +0000

    fix issue

--- recipify/urls.py
+++ recipify/urls.py
@@ -33,8 +33,8 @@ urlpatterns = [
     path('profile_page/', views.profile_page, name='profile_page'),
     path('create_recipe/', views.create_recipe, name='create_recipe'),
     path('all_recipes/', views.all_recipes, name='all_recipes'),
-    path('recipe/<int:recipe_id>/', views.get_recipe, name="get_recipe"),,
-    path('cupboard/', views.cupboard, name="cupboard")
+    path('recipe/<int:recipe_id>/', views.get_recipe, name="get_recipe"),
+    path('cupboard/', views.cupboard, name="cupboard"),
     path('select2/', include('django_select2.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
