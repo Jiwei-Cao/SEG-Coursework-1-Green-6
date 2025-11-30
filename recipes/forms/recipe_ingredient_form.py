0504fdc02529e00a467ccb3efd2231e1e879ea21
hafsabhudye <hafsa.bhudye@gmail.com>
2025-11-30 20:14:59 +0000

    Version with extra rows set to 10

--- recipes/forms/recipe_ingredient_form.py
+++ recipes/forms/recipe_ingredient_form.py
@@ -4,17 +4,8 @@ from recipes.models import RecipeIngredient
 
 class RecipeIngredientForm(forms.ModelForm):
 
-    # These tweaks are needed to
-    # prevent the user from changing the recipe of which they are editing ingredient
-    # allow the form to be submitted to the backend for processing
-    # the recipe is then explicitly set again the ingredients
-    def __init__(self, *args, **kwargs):
-        super(RecipeIngredientForm, self).__init__(*args, **kwargs)
-        self.fields['recipe'].disabled = True
-        self.fields['recipe'].required = False
-
     class Meta:
         model = RecipeIngredient
-        fields = ['recipe', 'quantity', 'unit', 'ingredient']
+        fields = ['quantity', 'unit', 'ingredient']
 
-RecipeIngredientFormSet = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=0, can_delete=True)
+RecipeIngredientFormSet = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=10, can_delete=True)
