b135c60a0ece38b8da61c0915b97bee297407d69
hafsabhudye <hafsa.bhudye@gmail.com>
2025-12-06 19:59:05 +0000

    add specify ingredient

---
+++ recipes/views/create_ingredient.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from recipes.forms.ingredient_form import IngredientForm
+
+@login_required
+def create_ingredient(request):
+    if request.method == 'POST':
+        form = IngredientForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            ingredient = form.save(commit=False)
+            ingredient.user = request.user
+            ingredient.save()
+            form.save()
+            return redirect('specify_ingredient')
+    else:
+        form = IngredientForm()
+    return render(request, 'specify_ingredient.html', {'form': IngredientForm})
