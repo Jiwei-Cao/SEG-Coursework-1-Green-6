f37fbd20d77238b235115c51d99e7a4c76082462
reubenweibel <reuben.weibel@kcl.ac.uk>
2025-12-11 19:24:22 +0000

    Refactored code

--- recipes/views/specific_recipe_view.py
+++ recipes/views/specific_recipe_view.py
@@ -96,45 +96,47 @@ def count_recipe_comments(recipe):
     counter = counter + recipe.comments.count()
     return counter
 
-def create_shopping_list(user, ingredients):
-    cupboard = UserIngredient.objects.filter(user = user)
-    cupboard_names = {ingredient.name for ingredient in cupboard}
+def convert_to_base(quantity, unit):
     unit_to_grams = {'gs': 1, 'lbs': 453.592, 'kgs':1000}
     unit_to_mls = {'tsps': 5, 'tbsps': 15, 'mls': 1, 'ltrs': 1000}
 
+    if unit in unit_to_grams:
+        return quantity * unit_to_grams[unit], 'mass'
+    if unit in unit_to_mls:
+        return quantity * unit_to_mls[unit], 'volume'
+    return quantity, 'other'
+
+
+def calculate_difference(recipe_ingredient, cupboard_ingredient):
+    recipe_qty, recipe_type = convert_to_base(recipe_ingredient.scaled_quantity, recipe_ingredient.unit.symbol)
+    cupboard_qty, cupboard_type =  convert_to_base(cupboard_ingredient.quantity, cupboard_ingredient.unit.symbol)
+    
+    if recipe_type != cupboard_type:
+        return recipe_ingredient.scaled_quantity
+    
+    if recipe_qty <= cupboard_qty:
+        return 0
+    
+    diff = recipe_qty - cupboard_qty
+
+    return diff / (recipe_qty / recipe_ingredient.scaled_quantity)
+
+
+def create_shopping_list(user, ingredients):
+    cupboard = {ingredient.name: ingredient for ingredient in UserIngredient.objects.filter(user = user)}
+
     shopping_list = []
     for ingredient in ingredients:
-        if ingredient.ingredient.name not in cupboard_names:
+        name = ingredient.ingredient.name
+
+        if name not in cupboard:
             ingredient.difference_quantity = ingredient.scaled_quantity
             shopping_list.append(ingredient)
             continue
 
-        cupboard_ingredient = cupboard.get(name=ingredient.ingredient.name)
-        try:
-            if ingredient.unit.symbol in unit_to_grams:
-                ingredient_qty = ingredient.scaled_quantity * unit_to_grams[ingredient.unit.symbol]
-                cupboard_qty = cupboard_ingredient.quantity * unit_to_grams[cupboard_ingredient.unit.symbol]
-            
-            elif ingredient.unit.symbol in unit_to_mls:
-                ingredient_qty = ingredient.scaled_quantity * unit_to_mls[ingredient.unit.symbol]
-                cupboard_qty = cupboard_ingredient.quantity * unit_to_mls[cupboard_ingredient.unit.symbol]
+        difference_qty = calculate_difference(ingredient, cupboard[name])
 
-            else:
-                ingredient_qty = ingredient.scaled_quantity
-                cupboard_qty = cupboard_ingredient.quantity
-
-        except:
-            ingredient.difference_quantity = ingredient.scaled_quantity
-            shopping_list.append(ingredient)
-            continue
-
-        if ingredient_qty > cupboard_qty:
-            difference_qty = ingredient_qty - cupboard_qty
-            if ingredient.unit.symbol in unit_to_grams:
-                ingredient.difference_quantity = difference_qty / unit_to_grams[ingredient.unit.symbol]
-            elif ingredient.unit.symbol in unit_to_mls:
-                ingredient.difference_quantity = difference_qty / unit_to_mls[ingredient.unit.symbol]
-            else:
-                ingredient.difference_quantity = difference_qty
+        if difference_qty > 0:
+            ingredient.difference_quantity = difference_qty
             shopping_list.append(ingredient)
     return shopping_list
